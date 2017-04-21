bd126a91f1aa667f2a73b1d67e68d022406803cd
Vladislav Lugovskoy <lugovskoy.v@gmail.com>
2017-04-21 04:23:39 +0300

    Minor network fix

--- Assets/Scripts/Network/NetworkController.cs
+++ Assets/Scripts/Network/NetworkController.cs
@@ -3,7 +3,8 @@
     using System.Net;
     public class NetworkController : MonoBehaviour {
         public int portBroadcast = 1500;
-        public int portGame = 1555;
+        public int portGameServer = 1555;
+        public int portGameClient = 1556;
         protected LanBroadcaster lanBc;
         protected Pong.Network.UdpHandler udpHandler;
         
@@ -16,7 +17,7 @@
             Debug.LogError("Starting Server");
             lanBc.StopBroadcasting();
             lanBc.StartAnnounceBroadcasting();
-            udpHandler = new Pong.Network.UdpHandler(portGame);
+            udpHandler = new Pong.Network.UdpHandler(portGameServer);
             udpHandler.StartListening();
             
         }
@@ -24,9 +25,11 @@
         private void ConnectToServer(string ip) {
             Debug.LogError("Connecting to server: " + ip);
             lanBc.StopBroadcasting();
-            udpHandler = new Pong.Network.UdpHandler(portGame);
+            udpHandler = new Pong.Network.UdpHandler(portGameClient);
             udpHandler.StartListening();
-            udpHandler.SendMessage(new Pong.Network.UdpMessage(new IPEndPoint(IPAddress.Parse(ip), portGame), new Pong.Network.CommandConnect()));
+            var endpoint = new IPEndPoint(IPAddress.Parse(ip), portGameServer);
+            var command = new Pong.Network.CommandConnect();
+            udpHandler.SendMessage(new Pong.Network.UdpMessage(endpoint, command));
         }
 
     }
